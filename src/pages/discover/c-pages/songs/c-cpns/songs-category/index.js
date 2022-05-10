e99e013028666da57ed7046579a4a9c5aa7db3dd
wxx <274855373.qq.com>
2022-05-10 15:56:05 +0800

    fix pagination data

--- src/pages/discover/c-pages/songs/c-cpns/songs-category/index.js
+++ src/pages/discover/c-pages/songs/c-cpns/songs-category/index.js
@@ -1,59 +1,63 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 import { useSelector, useDispatch, shallowEqual } from "react-redux";
 
 import {
   changeCurrentCategoryAction,
-  getSongList
+  getSongList,
 } from "../../store/actionCreators";
 
-import {
-  CategoryWrapper
-} from "./style";
+import { CategoryWrapper } from "./style";
 
 export default memo(function HYSongsCategory() {
   // redux
-  const { category } = useSelector(state => ({
-    category: state.getIn(["songs", "category"])
-  }), shallowEqual);
+  const { category } = useSelector(
+    (state) => ({
+      category: state.getIn(["songs", "category"]),
+    }),
+    shallowEqual
+  );
   const dispatch = useDispatch();
 
   function selectCategory(name) {
     dispatch(changeCurrentCategoryAction(name));
-    dispatch(getSongList(0));
+    dispatch(getSongList(1));
   }
 
   return (
     <CategoryWrapper>
       <div className="arrow sprite_icon"></div>
       <div className="all">
-        <span className="link" onClick={e => selectCategory("全部")}>全部风格</span>
+        <span className="link" onClick={(e) => selectCategory("全部")}>
+          全部风格
+        </span>
       </div>
       <div className="category">
-        {
-          category.map((item, index) => {
-            return (
-              <dl key={item.name} className={"item" + index}>
-                <dt>
-                  <i className="icon sprite_icon2"></i>
-                  <span>{item.name}</span>
-                </dt>
-                <dd>
-                  {
-                    item.subs.map(sItem => {
-                      return (
-                        <div className="item" key={sItem.name}>
-                          <span className="link" onClick={e => selectCategory(sItem.name)}>{sItem.name}</span>
-                          <span className="divider">|</span>
-                        </div>
-                      )
-                    })
-                  }
-                </dd>
-              </dl>
-            )
-          })
-        }
+        {category.map((item, index) => {
+          return (
+            <dl key={item.name} className={"item" + index}>
+              <dt>
+                <i className="icon sprite_icon2"></i>
+                <span>{item.name}</span>
+              </dt>
+              <dd>
+                {item.subs.map((sItem) => {
+                  return (
+                    <div className="item" key={sItem.name}>
+                      <span
+                        className="link"
+                        onClick={(e) => selectCategory(sItem.name)}
+                      >
+                        {sItem.name}
+                      </span>
+                      <span className="divider">|</span>
+                    </div>
+                  );
+                })}
+              </dd>
+            </dl>
+          );
+        })}
       </div>
     </CategoryWrapper>
-  )
-})
+  );
+});
--- src/pages/discover/c-pages/songs/store/actionCreators.js
+++ src/pages/discover/c-pages/songs/store/actionCreators.js
@@ -1,37 +1,32 @@
-import * as actionTypes from './constants';
-import { PER_PAGE_NUMBER } from './constants';
+import * as actionTypes from "./constants";
+import { PER_PAGE_NUMBER } from "./constants";
 
-import {
-  getSongCategory,
-  getSongCategoryList
-} from "@/services/songs";
-import { 
-  handleSongsCategory
-} from "@/utils/handle-data";
+import { getSongCategory, getSongCategoryList } from "@/services/songs";
+import { handleSongsCategory } from "@/utils/handle-data";
 
 const changeCategoryAction = (res) => ({
   type: actionTypes.CHANGE_CATEGORY,
-  category: res
-})
+  category: res,
+});
 
 const changeSongListAction = (res) => ({
   type: actionTypes.CHANGE_CATEGORY_SONGS,
-  categorySongs: res
-})
+  categorySongs: res,
+});
 
 export const changeCurrentCategoryAction = (name) => ({
   type: actionTypes.CHANGE_CURRENT_CATEGORY,
-  currentCategory: name
-})
+  currentCategory: name,
+});
 
 export const getCategory = () => {
-  return dispatch => {
-    getSongCategory().then(res => {
+  return (dispatch) => {
+    getSongCategory().then((res) => {
       const categoryData = handleSongsCategory(res);
-      dispatch(changeCategoryAction(categoryData))
-    })
-  }
-}
+      dispatch(changeCategoryAction(categoryData));
+    });
+  };
+};
 
 export const getSongList = (page) => {
   return (dispatch, getState) => {
@@ -39,8 +34,8 @@ export const getSongList = (page) => {
     const name = getState().getIn(["songs", "currentCategory"]);
 
     // 2.获取数据
-    getSongCategoryList(name, page * PER_PAGE_NUMBER).then(res => {
+    getSongCategoryList(name, (page - 1) * PER_PAGE_NUMBER).then((res) => {
       dispatch(changeSongListAction(res));
-    })
-  }
-}
+    });
+  };
+};
