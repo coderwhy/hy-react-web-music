43f8ac7752b465ef8ca6611add46a5e179796932
wxx <274855373.qq.com>
2022-05-10 16:15:20 +0800

    fix top banners

--- src/pages/discover/c-pages/recommend/c-cpns/top-banner/style.js
+++ src/pages/discover/c-pages/recommend/c-cpns/top-banner/style.js
@@ -1,16 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const BannerWrapper = styled.div`
-  background: url(${props => props.bgImage}) center center/6000px;
+  background: url(${(props) => props.bgImage}) center center/6000px;
 
   .banner {
     height: 270px;
-    background-color: red;
-
     display: flex;
     position: relative;
   }
-`
+`;
 
 export const BannerLeft = styled.div`
   width: 730px;
@@ -22,26 +20,22 @@ export const BannerLeft = styled.div`
       width: 100%;
     }
   }
-`
+`;
 
 export const BannerRight = styled.a.attrs({
   href: "https://music.163.com/#/download",
-  target: "_blank"
+  target: "_blank",
 })`
   width: 254px;
   height: 270px;
   background: url(${require("@/assets/img/download.png")});
-`
+`;
 
 export const BannerControl = styled.div`
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-
   .btn {
     position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
     width: 37px;
     height: 63px;
     background-image: url(${require("@/assets/img/banner_sprite.png")});
@@ -49,7 +43,7 @@ export const BannerControl = styled.div`
     cursor: pointer;
 
     &:hover {
-      background-color: rgba(0, 0, 0, .1);
+      background-color: rgba(0, 0, 0, 0.1);
     }
   }
 
@@ -62,4 +56,4 @@ export const BannerControl = styled.div`
     right: -68px;
     background-position: 0 -508px;
   }
-`
+`;
